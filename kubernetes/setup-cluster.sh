0ee658d6d4caecff4ae938a6330460f8886ffdf9
Tobias Trinks <tobias.trinks@code.berlin>
2019-02-14 17:45:17 +0100

    Fix certmanager installation and letsencrypt staging acme endpoint

--- kubernetes/setup-cluster.sh
+++ kubernetes/setup-cluster.sh
@@ -10,11 +10,24 @@ helm init --service-account tiller
 
 sleep 15
 
+# Install the CustomResourceDefinition resources separately
+kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+
+# Create the namespace for cert-manager
+kubectl create namespace cert-manager
+
+# Label the cert-manager namespace to disable resource validation
+kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
 helm install \
 --name cert-manager \
---namespace kube-system \
+--namespace cert-manager \
+--version v0.6 \
 stable/cert-manager \
---set ingressShim.defaultIssuerName=letsencrypt-prod \
 --set ingressShim.defaultIssuerKind=ClusterIssuer
 
 kubectl apply -f clusterissuers/letsencrypt-staging.yml
