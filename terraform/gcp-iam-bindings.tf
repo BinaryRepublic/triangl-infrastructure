72143a2eba55804d82da4bee49b2c7851efda2a9
Tobias Trinks <tobias.trinks99@gmail.com>
2019-11-26 13:26:51 +0100

    Restrict GCP datastore access rights

--- terraform/gcp-iam-bindings.tf
+++ terraform/gcp-iam-bindings.tf
@@ -10,3 +10,25 @@ resource "google_project_iam_binding" "cloud-sql-users" {
     "serviceAccount:cloud-sql-proxy@triangl.iam.gserviceaccount.com"
   ]
 }
+
+resource "google_project_iam_binding" "datastore-admin-user" {
+  depends_on = [
+    google_service_account.processing-pipeline,
+    google_service_account.tracking-ingestion-service
+  ]
+  role    = "roles/datastore.indexAdmin"
+  members  = [
+    "serviceAccount:processing-pipeline@triangl.iam.gserviceaccount.com",
+    "serviceAccount:tracking-ingestion-service@triangl.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "datastore-customer-user" {
+  depends_on = [
+    google_service_account.customer-service
+  ]
+  role    = "roles/datastore.indexAdmin;datastore.entities.Customer"
+  members  = [
+    "serviceAccount:customer-service@triangl.iam.gserviceaccount.com",
+  ]
+}
